949337c01feb182af5f6855ffbe66798d901e659
Edward Hayashi <edwardhayashi7266@gmail.com>
2024-05-02 19:13:27 -0400

    SpringBoot Initialized

---
+++ src/test/java/cs4370/unibooks/UniBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package cs4370.unibooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
